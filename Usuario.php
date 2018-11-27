f7e150db0886737b7ae7693fa6edd4c4b7a235b1
aled35 <dianahuer35@outlook.com>
2018-11-27 15:00:23 -0600

    Me gusta el mexicano
    Mexicano Quisiera ser la rama en la que se para tu pajarito.

---
+++ Usuario.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Inicio</title>
+
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+
+    <style>
+
+    </style>
+</head>
+
+<body>
+
+<header>
+    <section name="index">
+        <a href="Usuario.php"><img id="logo" src="img/escom.png"></a>
+        <h2><a href="Usuario.php">Estacionamiento ESCOM</a></h2>
+    </section>
+    <section name="user">
+        <nav>
+            <ul>
+                <li><a href="mapa1.html">Mapa</a></li>
+                <li><a href="reportarFalta.html">Reportar falta</a></li>
+                <li><a href="registraCarro.html">Registrar Auto</a></li>
+            </ul>
+        </nav>
+        <!--------------------------------------------------------------------------------------------------->
+        <div class="sesion2">
+            <button class="logout"><a href="logout.php">Cerrar Sesión</a></button>
+        </div>
+    </section>
+</header>
+
+<main>
+</main>
+
+<footer>
+    <p>Valencia Rodriguez Fernando</p>
+    <p>Azpeitia Hernánez Vladimir </p>
+    <p>Huerta Alvarez Diana Alejandra</p>
+</footer>
+
+
+</body>
+</html>
--- login.php
+++ login.php
@@ -6,13 +6,18 @@
 	$password = $_POST['pass1'];
 
 	$query = "SELECT * FROM tb_conductor WHERE IDConductor ='".$nickname."' AND password ='".$password."'";
-
-
+ 
 	if($resultado = $conexion->query($query)){
 		if($resultado->num_rows>0)
 		{
-			$_SESSION["user1"] = $nickname;
-			header("Location: inicioconlogin.php", true, 301);
+			$ret = mysqli_fetch_array($resultado);
+			$tipo = $ret['tipo']; 
+			$_SESSION["user"] = $nickname;
+			$_SESSION["tipo"] = $tipo;
+			if($tipo  == "Admin")
+				header("Location: Administrador.php", true, 301);
+			else
+				header("Location: Usuario.php", true, 301);	
 			exit();
 		}else{
 			echo'<script>alert("Usuario no valido");</script>';
