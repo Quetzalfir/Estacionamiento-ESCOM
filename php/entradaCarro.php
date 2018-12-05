308b3921891aefd7ef2bf9b1bdb28609fc403f7e
VladimirAh <vladisazp@gmail.com>
2018-12-04 22:19:18 -0600

    Registro bitacora

--- php/entradaCarro.php
+++ php/entradaCarro.php
@@ -1,9 +1,14 @@
 <?php 
 	include('config.php');
 	session_start();
-    $consulta = "INSERT INTO `tb_bitacora`(`horaEntrada`, `noCajon`, `IDConductor`, `fecha`, `sancion`, `IDVigilante`) VALUES ('".$_POST['horaEntrada']."','".$_POST['noCajon']."','','','','')";
-    $resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta a la base de datos");
-	header("Location: ../tablaUsuarios.php", true, 301);
-    exit();
+	$query = "SELECT `IDConductor` FROM `tb_automovil` WHERE `placas` = '".$_POST['placas']."'";
+	if($res = $conexion->query($query))
+		if($res->num_rows>0)
+			$ret = mysqli_fetch_array($res);
+	
+    $consulta = "INSERT INTO `tb_bitacora`(`horaEntrada`, `noCajon`, `IDConductor`, `fecha`, `sancion`, `IDVigilante`) VALUES ('".$_POST['horaEntrada']."','".$_POST['noCajon']."','".$ret['IDConductor']."','". date("Y/n/j") ."','0','".$_SESSION['user']."')";
+    mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta a la base de datos");
+	header("Location: ../bitacoraVig.php", true, 301);
     mysqli_close( $conexion );
+    exit();
  ?>
